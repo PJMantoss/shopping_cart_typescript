d9c851606576b6b67afed0302500abf65f5b261b
PJMantoss <joelptoss@gmail.com>
2021-06-17 17:57:03 +0100

    update styling for Item component

--- src/Item/Item.styles.ts
+++ src/Item/Item.styles.ts
@@ -1,3 +1,9 @@
 import styled from 'styled-components';
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    width: 100%;
+    border: 1px solid lightblue;
+`;
