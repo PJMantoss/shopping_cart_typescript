26a7e59f73cf29433bcdfd18d2d9c31e24c4fe96
PJMantoss <joelptoss@gmail.com>
2021-06-17 14:59:34 +0100

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,11 @@ const getProducts = async (): Promise<CartItemType[]> =>
   await (await fetch('https://fakestoreapi.com/products')).json()
 
 const App = () => {
-  const {data, isLoading, error} = useQuery<CartItemType[]>('products', getProducts);
+  const {data, isLoading, error} = useQuery<CartItemType[]>(
+    'products', getProducts
+    );
+
+    console.log(data);
 
   return (
     <div className="App">
