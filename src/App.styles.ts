60bceb0b4f56e14f24788856533641be28e66240
PJMantoss <joelptoss@gmail.com>
2021-06-17 18:39:08 +0100

    update App component

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -1,3 +1,6 @@
 import styled from 'styled-components';
+import IconButton from '@material-ui/core/IconButton';
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+    margin: 40px;
+`;
