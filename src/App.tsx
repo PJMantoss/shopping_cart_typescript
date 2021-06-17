26f5ee5f367e2896c34d1e3d11ddaca29e46067d
PJMantoss <joelptoss@gmail.com>
2021-06-17 18:44:21 +0100

    update styling for App component

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import Grid from '@material-ui/core/Grid';
 import AddShopingCartIcon from '@material-ui/icons/AddShoppingCart';
 import Badge from '@material-ui/core/Badge';
 //Styles
-import { Wrapper } from './App.styles';
+import { Wrapper, StyledButton } from './App.styles';
 //Types of properties we'll be fetching from the FakeStore API
 export type CartItemType = {
   id: number;
