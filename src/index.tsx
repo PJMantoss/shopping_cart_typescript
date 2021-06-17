b9b0b35c0180a5d7f54129d8f7658ae738ccb67d
PJMantoss <joelptoss@gmail.com>
2021-06-17 14:22:57 +0100

    update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,4 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
+const client = new QueryClient();
+
 ReactDOM.render(<App />, document.getElementById('root'));
