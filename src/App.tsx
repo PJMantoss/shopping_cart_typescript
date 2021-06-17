36998de05a7d5807b281faf01b1a483f8b05f626
PJMantoss <joelptoss@gmail.com>
2021-06-17 14:43:20 +0100

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,10 @@ export type CartItemType = {
   id: number;
   category: string;
   description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
 }
 
 const App = () => {
