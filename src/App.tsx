2b1b222d2528493cf4148b23cc8a9b7b7fd6c57c
PJMantoss <joelptoss@gmail.com>
2021-06-17 14:53:14 +0100

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -19,9 +19,8 @@ export type CartItemType = {
   amount: number;
 }
 
-const getProducts = async () => {
+const getProducts = async (): Promise<CartItemType> =>
   await (await fetch('https://fakestoreapi.com/products')).json()
-}
 
 const App = () => {
   return (
