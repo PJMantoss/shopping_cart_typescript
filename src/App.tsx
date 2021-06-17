290556bd323e6a8469f767ae7d2a37ae4070d19f
PJMantoss <joelptoss@gmail.com>
2021-06-17 14:29:46 +0100

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import { useState } from 'react';
 import { useQuery } from 'react-query';
+//Components
+import Drawer from '@material-ui/core/Drawer';
+import LinearProgress from '@material-ui/core/LinearProgress';
 
 const App = () => {
   return (
