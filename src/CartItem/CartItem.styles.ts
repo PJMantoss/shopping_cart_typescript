2c1b6ae6edd0d41277ef271d602203d08b800304
PJMantoss <joelptoss@gmail.com>
2021-06-24 12:58:23 +0100

    update CartItem

--- src/CartItem/CartItem.styles.ts
+++ src/CartItem/CartItem.styles.ts
@@ -4,4 +4,6 @@ export const Wrapper = styled.div`
     display: flex;
     justify-content: space-between;
     font-family: Arial, Helvetica, sans-serif;
+    border-bottom: 1px solid lightblue;
+    padding-bottom: 20px;
 `;
