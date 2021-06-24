fb071df98e9f39bfeb7ea4aa83a479a21d922906
PJMantoss <joelptoss@gmail.com>
2021-06-24 11:42:27 +0100

    update Cart

--- src/Cart/Cart.tsx
+++ src/Cart/Cart.tsx
@@ -15,14 +15,14 @@ const Cart: React.FC<Props> = ({ cartItems, addToCart, removeFromCart }) => {
         <Wrapper>
             <h2>Your Shopping Cart</h2>
             {cartItems.length === 0 ? <p>No items in cart.</p> : null}
-            {/* {cartItems.map(item => (
+            {cartItems.map(item => (
                 <CartItem 
                     key = {item.id} 
                     item = {item} 
                     addToCart = {addToCart} 
                     removeFromCart = {removeFromCart} 
                 />
-            ))} */}
+            ))}
         </Wrapper>
     )
 }
