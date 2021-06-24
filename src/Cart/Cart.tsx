1c60904e01e8bb810dcbbbaa25bc05ffb0384cdc
PJMantoss <joelptoss@gmail.com>
2021-06-24 13:33:07 +0100

    update Cart

--- src/Cart/Cart.tsx
+++ src/Cart/Cart.tsx
@@ -10,20 +10,25 @@ type Props = {
     removeFromCart: (id: number) => void;
 }
 
-const Cart: React.FC<Props> = ({ cartItems, addToCart, removeFromCart }) => (
+const Cart: React.FC<Props> = ({ cartItems, addToCart, removeFromCart }) => {
+
+    const calculate = (items: CartItemType[]) => 
+        items.reduce((acc: number, item) => acc + item.amount * item.price, 0);
         
-        <Wrapper>
-            <h2>Your Shopping Cart</h2>
-            {cartItems.length === 0 ? <p>No items in cart.</p> : null}
-            {cartItems.map(item => (
-                <CartItem 
-                    key = {item.id} 
-                    item = {item} 
-                    addToCart = {addToCart} 
-                    removeFromCart = {removeFromCart} 
-                />
-            ))}
-        </Wrapper>
-    )
+        return(
+            <Wrapper>
+                <h2>Your Shopping Cart</h2>
+                {cartItems.length === 0 ? <p>No items in cart.</p> : null}
+                {cartItems.map(item => (
+                    <CartItem 
+                        key = {item.id} 
+                        item = {item} 
+                        addToCart = {addToCart} 
+                        removeFromCart = {removeFromCart} 
+                    />
+                ))}
+            </Wrapper>
+        )
+    }
 
 export default Cart;
